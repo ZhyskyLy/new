8987d8ff6660b87fad39244ec03801cba121c358
ZhyskyLy <millerlds5@gmail.com>
2022-09-20 09:53:02 -0500

    Correcciones 1.0.

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app=Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('sitio/index.html')
+    return render_template('Sitio/index.html')
 
 @app.route('/Nosotros')
 def Nosotros():
-    return render_template('sitio/Nosotros.html')
+    return render_template('Sitio/Nosotros.html')
 
 @app.route('/Servicios')
 def Servicios():
-    return render_template('sitio/Servicios.html')
+    return render_template('Sitio/Servicios.html')
 
 @app.route('/Contacto')
 def Contacto():
-    return render_template('sitio/Contacto.html')
+    return render_template('Sitio/Contacto.html')
 
 
 if __name__ =='__main__':
